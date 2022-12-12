1a791264307ecdc88240f1e9fd5dbdce0441e4c3
Ana Bujan <ana@eisberg-labs.com>
2022-12-12 15:48:31 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import nodeGlobals from 'rollup-plugin-node-globals';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import terser  from '@rollup/plugin-terser';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import replace from '@rollup/plugin-replace';
 import * as fs from "fs";
@@ -16,21 +16,23 @@ const commonjsOptions = {
   ignoreGlobal: true,
   include: /node_modules/,
 }
+const extensions = ['.js', '.ts', '.tsx'];
+
 const babelOptions = {
   exclude: /node_modules/,
-  extensions: ['.js', '.ts', '.tsx'],
+  extensions,
   configFile: '../../babel.config.json',
   babelHelpers: 'runtime'
 };
 const nodeOptions = {
-  extensions: ['.js', '.tsx', '.ts'],
+  extensions,
 };
 const typescriptOptions = {
   tsconfig: `${PACKAGE_NAME}/tsconfig.json`,
   declaration: true,
   declarationDir: '.',
-  "emitDeclarationOnly": true,
-  "declarationMap": true,
+  emitDeclarationOnly: true,
+  declarationMap: true,
 };
 
 export default {
