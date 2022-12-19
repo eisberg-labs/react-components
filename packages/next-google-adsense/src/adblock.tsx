e910d53f7003c86f53c6a96e95866032a7c7cc8f
Ana Bujan <ana@eisberg-labs.com>
2022-12-19 17:28:17 +0100

    feat(next-google-adsense): add next-google-adsense

---
+++ packages/next-google-adsense/src/adblock.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+
+export type Props = React.InsHTMLAttributes<HTMLModElement>;
+export default function Adblock(props: Props) {
+  useEffect(() => {
+    try {
+      if (window.hasOwnProperty('adsbygoogle')) {
+        // @ts-ignore
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      }
+    } catch (e) {
+      console.error('Could not initialize adsense ad block', e);
+    }
+  }, []);
+
+  return <ins {...props}></ins>;
+}
---
+++ packages/next-google-adsense/src/index.ts
@@ -0,0 +1,4 @@
+import Adsense, { Props as AdsenseProps } from './adsense';
+import Adblock, { Props as AdblockProps } from './adblock';
+export { Adblock, AdsenseProps, AdblockProps };
+export default Adsense;
