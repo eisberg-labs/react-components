3c6f93c506d0d33d77554dd9ee9fae9ba2fce1c5
Ana Bujan <ana@eisberg-labs.com>
2023-01-03 11:57:32 +0100

    feat(use-google-charts): add new package use-google-charts

---
+++ packages/use-google-charts/__tests__/use-google-charts.test.tsx
@@ -0,0 +1,9 @@
+/**
+ * @jest-environment jsdom
+ */
+import '@testing-library/jest-dom/extend-expect';
+import * as React from 'react';
+
+test('should render', () => {
+  expect(1).toBe(1);
+});
---
+++ packages/use-google-charts/src/index.ts
@@ -0,0 +1,2 @@
+import useGoogleCharts from './use-google-charts';
+export { useGoogleCharts };
